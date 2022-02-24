46347b74817f2325ed0c62fa39c72b0ecb60d42e
Masviol <89834255+Masviol@users.noreply.github.com>
2022-02-24 14:45:52 +0300

    Create .h

---
+++ lab1/.h
@@ -0,0 +1,33 @@
+//.h FILES
+
+//my.h
+
+#ifndef INC_2_1_MY_H
+#define INC_2_1_MY_H
+
+void proga();
+
+#endif //INC_2_1_MY_H
+
+//swap.h
+
+#ifndef INC_2_1_SWAP_H
+#define INC_2_1_SWAP_H
+
+void swap_uk(int a, int b);
+
+void swap_s(int &a, int &b);
+
+void num_int_uk(float a, float b);
+
+void num_int_s(float &a, float &b);
+
+void cmplex(int &z1, int &z2, int &num);
+
+void cmplex_uk(int z1, int z2, int num);
+
+void mcircle_uk(int *rx, int *ry, int  *prx, int *pry);
+
+void mcircle_s(int &rx, int &ry, int &prx, int &pry);
+
+#endif //INC_2_1_SWAP_H
